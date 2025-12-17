77ff15371c8ec802245b17303c5a29e79e9c0f80
Marcelo Andrade da Silva <marcelo.andradesilva26@gmail.com>
2025-12-17 15:39:14 -0300

    Add files via upload

---
+++ Nokia.cs
@@ -0,0 +1,16 @@
+namespace DesafioPOO.Models
+{
+    // TODO: Herdar da classe "Smartphone"
+    public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string iMEI, int memoria) : base(numero, modelo, iMEI, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no {Modelo}");
+        }
+        // TODO: Sobrescrever o método "InstalarAplicativo"
+    }
+}
