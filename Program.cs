0dd7bcaa7e0a097b9fb7bf6695ba15c279931d7e
Marcelo Andrade da Silva <marcelo.andradesilva26@gmail.com>
2025-12-17 15:42:59 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia  = new Nokia(numero: "978650043", modelo:"Nokia k9", iMEI:"435785", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "978660043", modelo: "Iphone 17 Pro Max", iMEI: "789324", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
